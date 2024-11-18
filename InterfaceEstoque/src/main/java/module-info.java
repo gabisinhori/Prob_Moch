dabe8d64699525eb538c33a2a384216376793e04
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-11-18 20:14:53 -0300

    Removendo a versão anterior

--- InterfaceEstoque/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.interfaceestoque {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.interfaceestoque to javafx.fxml;
-    exports com.example.interfaceestoque;
-}
