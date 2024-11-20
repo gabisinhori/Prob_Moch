6d6654c873f3a025940f03f6d7d1bcff956b570a
rafaeljuniora <160675357+rafaeljuniora@users.noreply.github.com>
2024-11-19 21:17:22 -0300

    Add files via upload

--- InterfaceEstoque/src/main/java/com/example/interfaceestoque/AlgoritimoAEstrela.java
+++ InterfaceEstoque/src/main/java/com/example/interfaceestoque/AlgoritimoAEstrela.java
@@ -41,43 +41,4 @@ public class AlgoritimoAEstrela {
     }
 }
 
-class Estado {
-    private List<Produto> produtos;
-    private int pesoTotal;
-    private double valorTotal;
 
-    public Estado(List<Produto> produtos, int pesoTotal, double valorTotal) {
-        this.produtos = produtos;
-        this.pesoTotal = pesoTotal;
-        this.valorTotal = valorTotal;
-    }
-
-    public List<Produto> getProdutos() {
-        return produtos;
-    }
-
-    public int getPesoTotal() {
-        return pesoTotal;
-    }
-
-    public double getValorTotal() {
-        return valorTotal;
-    }
-
-    public double getF() {
-        return valorTotal;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof Estado)) return false;
-        Estado other = (Estado) obj;
-        return produtos.equals(other.produtos);
-    }
-
-    @Override
-    public int hashCode() {
-        return produtos.hashCode();
-    }
-}
---
+++ InterfaceEstoque/src/main/java/com/example/interfaceestoque/Estado.java
@@ -0,0 +1,45 @@
+package com.example.interfaceestoque;
+
+import java.util.List;
+
+public class Estado {
+        private List<Produto> produtos;
+        private int pesoTotal;
+        private double valorTotal;
+
+        public Estado(List<Produto> produtos, int pesoTotal, double valorTotal) {
+            this.produtos = produtos;
+            this.pesoTotal = pesoTotal;
+            this.valorTotal = valorTotal;
+        }
+
+        public List<Produto> getProdutos() {
+            return produtos;
+        }
+
+        public int getPesoTotal() {
+            return pesoTotal;
+        }
+
+        public double getValorTotal() {
+            return valorTotal;
+        }
+
+        public double getF() {
+            return valorTotal;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (!(obj instanceof com.example.interfaceestoque.Estado)) return false;
+            com.example.interfaceestoque.Estado other = (com.example.interfaceestoque.Estado) obj;
+            return produtos.equals(other.produtos);
+        }
+
+        @Override
+        public int hashCode() {
+            return produtos.hashCode();
+        }
+    }
+}
